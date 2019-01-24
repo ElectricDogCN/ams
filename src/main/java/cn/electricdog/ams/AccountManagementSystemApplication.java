3a86bebe5d2d4eadeb092048c24ecaddb6fc019d
ElectricDog <suruifeng98@gmail.com>
2019-01-25 00:39:23 +0800

    Create Account Management System. I hope this is the first step to success. Over.

---
+++ src/main/java/cn/electricdog/ams/AccountManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package cn.electricdog.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot的入口函数
+ * @author 13997
+ */
+@SpringBootApplication
+public class AccountManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementSystemApplication.class, args);
+	}
+
+}
+
